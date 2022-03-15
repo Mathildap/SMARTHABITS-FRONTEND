274d37d45aa7ac767a6959d63540e52252cb90d2
Mathilda Pettersson <mathilda.pettersson@medieinstitutet.se>
2022-03-15 14:32:29 +0100

    :memo: created react-app and deleted unnecessary files, downloaded npm packages, created sass folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
+import './css/normalize.css';
+import date from 'date-and-time';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className='App'>SMARTHABITS</div>;
 }
 
 export default App;
